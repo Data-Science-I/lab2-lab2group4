3af26c4fa423327c779231eabfe7adcc67bddc2b
Yiyan Yang <yyyyyy48@163.com>
2024-10-29 20:04:42 -0400

    Test1

--- group4/R/package.R
+++ group4/R/package.R
@@ -1,15 +1,3 @@
-# load packages
-library(devtools)
-library(roxygen2)
-library(tidyverse)
-library(here)
-
-# read in csv file
-df <- read_csv(here("R/DRG_data.csv"))
-
-# make substring of the DRG codes
-df <- df %>% mutate(DRG = substr(`DRG Definition`, 1, 3))
-
 #' Title: Create a Boxplot by DRG
 #'
 #' This function takes one string and returns a boxplot
@@ -22,24 +10,24 @@ df <- df %>% mutate(DRG = substr(`DRG Definition`, 1, 3))
 #' @export
 
 # function where the argument is "covered_charges", "total payments" or "medicare payments"
-boxplot_DRG <- function(col_name) {
+boxplot_DRG <- function(data,col_name) {
   if (col_name == "covered_charges") {
     # make ggplot for covered_charges
-    ggplot(df, aes(x = DRG,
+    ggplot(data, aes(x = DRG,
                    y = `Average Covered Charges`)) +
       geom_boxplot() + # boxplot
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 90, hjust = 1)) # makes the titles vertical
   } else if (col_name == "total_payments") {
     # make ggplot for total payments
-    ggplot(df, aes(x = DRG,
+    ggplot(data, aes(x = DRG,
                    y = `Average Total Payments`)) +
       geom_boxplot() +
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))  # makes the titles vertical
   } else if (col_name == "medicare_payments") {
     # make ggplot for medicare payments
-    ggplot(df, aes(x = DRG,
+    ggplot(data, aes(x = DRG,
                    y = `Average Medicare Payments`)) +
       geom_boxplot() +
       theme_minimal() +
