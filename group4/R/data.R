70aa3490ef4955bea2e758aa8c4e5a3a3aa2c0dd
Yiyan Yang <yyyyyy48@163.com>
2024-10-30 17:03:49 -0400

    .

---
+++ group4/R/data.R
@@ -0,0 +1,8 @@
+#' @title DERG
+#' @description DRG
+#' @format A data frame with 163065 rows and 12 columns.
+#' @source source("~/Desktop/Data Science/lab/lab2-lab2group4/group4/R/data.R")
+#' @export
+"data"
+
+
